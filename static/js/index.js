6a5f37c86bfa184fc8ece57f39f7e07dfa7fe3e3
overleo007 <Sokolov_lev2003@mail.ru>
2023-03-09 11:45:20 +0700

    75% done

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,41 @@
-// (function () {
-//     'use strict'
-//
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.querySelectorAll('.needs-validation')
-//
-//     // Loop over them and prevent submission
-//     Array.prototype.slice.call(forms)
-//         .forEach(function (form) {
-//             form.addEventListener('submit', function (event) {
-//                 if (!form.checkValidity()) {
-//                     event.preventDefault()
-//                     event.stopPropagation()
-//                 }
-//
-//                 form.classList.add('was-validated')
-//             }, false)
-//         })
-// })()
+print = console.log
+
+const form = document.getElementById('myForm');
+
+let inputs = [];
+
+for (let i = 0; i < form.elements.length; i++) {
+    let cur_elem = form.elements[i];
+    if (cur_elem.name !== 'name') {
+        cur_elem.disabled = true;
+    }
+    print(cur_elem.tagName)
+    inputs.push(cur_elem)
+}
+print(inputs)
+
+for (let i = 0; i < inputs.length - 1; i++) {
+    inputs[i].addEventListener('input', (ev) => {
+        if (ev.target.checkValidity()) {
+            inputs[i + 1].disabled = false;
+            ev.target.classList.remove("is-invalid")
+            ev.target.classList.add("is-valid")
+        } else {
+            ev.target.classList.add("is-invalid")
+        }
+    })
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // предотвратить отправку формы на сервер
+    let new_client = {};
+    // получить данные формы
+    const formData = new FormData(event.target);
+    for (let [key, value] of formData.entries()) {
+        print(key, value)
+        // const element = document.getElementById('key');
+        new_client[key] = value;
+    }
+
+    print(new_client);
+});
