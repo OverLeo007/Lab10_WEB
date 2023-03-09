a67e6d204c56a4c38977be442024e72a5f32a3f1
overleo007 <Sokolov_lev2003@mail.ru>
2023-03-09 13:37:30 +0700

    99% done

--- static/js/index.js
+++ static/js/index.js
@@ -9,21 +9,38 @@ for (let i = 0; i < form.elements.length; i++) {
     if (cur_elem.name !== 'name') {
         cur_elem.disabled = true;
     }
-    print(cur_elem.tagName)
     inputs.push(cur_elem)
 }
-print(inputs)
+let disables = 0
+updateButtonColor(disables, inputs.length)
 
 for (let i = 0; i < inputs.length - 1; i++) {
     inputs[i].addEventListener('input', (ev) => {
         if (ev.target.checkValidity()) {
             inputs[i + 1].disabled = false;
-            ev.target.classList.remove("is-invalid")
-            ev.target.classList.add("is-valid")
+            ev.target.classList.remove("is-invalid");
+            ev.target.classList.add("is-valid");
         } else {
-            ev.target.classList.add("is-invalid")
+            ev.target.classList.add("is-invalid");
+            inputs[i + 1].disabled = true;
         }
+        disables = inputs.filter(obj => obj.disabled === false).length
+        updateButtonColor(disables - 1, inputs.length - 1)
     })
+}
+
+function updateButtonColor(disablesCount, inputsLen) {
+    let percentValid = disablesCount / inputsLen * 100;
+    form['submit'].style.backgroundImage = `linear-gradient(to right, #0d6efd ${percentValid}%, white ${percentValid}% 100%)`;
+    persInfo = document.getElementById("percentage")
+    persInfo.innerText = `Заполнено на ${Math.round(percentValid)}%`;
+    if (Math.round(percentValid) === 100) {
+        persInfo.classList.remove('text-info')
+        persInfo.classList.add('text-success')
+    } else {
+        persInfo.classList.remove('text-success')
+        persInfo.classList.add('text-info')
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -32,10 +49,20 @@ form.addEventListener('submit', (event) => {
     // получить данные формы
     const formData = new FormData(event.target);
     for (let [key, value] of formData.entries()) {
-        print(key, value)
-        // const element = document.getElementById('key');
         new_client[key] = value;
     }
 
     print(new_client);
+    form.reset();
+    inputs = [];
+    for (let i = 0; i < form.elements.length; i++) {
+        let cur_elem = form.elements[i];
+        cur_elem.classList.remove("is-valid");
+        if (cur_elem.name !== 'name') {
+            cur_elem.disabled = true;
+        }
+        inputs.push(cur_elem);
+    }
+    alert("Форма отправлена!");
 });
+
